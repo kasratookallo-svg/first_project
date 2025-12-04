354ff26c281f13d6054d912bb90ef0ef65b80827
Kasra Tookallo <kasratookallo@gmail.com>
2025-12-04 22:17:28 +0330

    Update supermarket_dao.py

--- supermarket_app/supermarket_dao.py
+++ supermarket_app/supermarket_dao.py
@@ -1,26 +1,27 @@
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 #                               Supermarket Application
 #                <<<<<<         Made by Kasra Tookallo           >>>>>>
 #                                   2025 the year
 #                                    11/28/2025
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 # Description : This program relies on two main approach simultaneously, including Class_Method (Object_Oriented programming) and Function_handling.
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 # Additional hint : Database is recalling Class_method (1st approach)
 #                   while
 #                   List_Features, including Submit and Total Price List through Window,
 #                   are based on Function_method (2nd approach).
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 # Finally, please read the following structions before running the perogram.
 # In List_Features there are two groups of Buttons in Window (tkinter):
 # The first group is Add to list and Total Price List.
 # The second group is Database_Related Buttons, including Submit, Edit , Remove to/from Database.
 # These two groups work independantly.
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+
 
 import sqlite3
 
-
+#                                             Database_related Functions 
 class ProductDataAccess:
     def save(self, product):
         with  sqlite3.connect("supermarket_db") as connection:
